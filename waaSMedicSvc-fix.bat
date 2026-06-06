3adeab8d6b2dca1988c41578f46993acd5806aff
Cris P <105011867+CriBP@users.noreply.github.com>
2026-06-05 22:39:44 -0700

    Delete waaSMedicSvc-fix.bat

--- waaSMedicSvc-fix.bat
+++
@@ -1,3 +0,0 @@
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\MUI\StringCacheSettings" /f /v "StringCacheGeneration" /t REG_DWORD /d 0000038b
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /f /v "Description" /t REG_SZ /d "Enables remediation and protection of Windows Update components."
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /f /v "DisplayName" /t REG_SZ /d "Windows Update Medic Service"
