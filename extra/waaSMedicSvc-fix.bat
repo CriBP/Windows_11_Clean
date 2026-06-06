906ade28f937957bae0b360737a8917e96078b4e
Cris P <105011867+CriBP@users.noreply.github.com>
2026-06-05 23:21:13 -0700

    Update waaSMedicSvc-fix.bat
    add explanations

--- extra/waaSMedicSvc-fix.bat
+++ extra/waaSMedicSvc-fix.bat
@@ -1,3 +1,4 @@
+:: Looking at the Services list, I found a service with broken description, here is the fix
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\MUI\StringCacheSettings" /f /v "StringCacheGeneration" /t REG_DWORD /d 0000038b
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /f /v "Description" /t REG_SZ /d "Enables remediation and protection of Windows Update components."
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /f /v "DisplayName" /t REG_SZ /d "Windows Update Medic Service"
